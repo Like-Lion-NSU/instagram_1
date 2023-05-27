3befc9bd749b436c93d1388c60a3022c460c07d8
정재은 <jjyjjy0528@naver.com>
2023-05-28 03:11:43 +0900

    어휴

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NewComponent from './jaeeun';
+
+function App() {
+  return (
+  <React.StrictMode>
+    <NewComponent />
+  </React.StrictMode>
+  );
+}
+
+export default App;
---
+++ src/jaeeun.js
@@ -0,0 +1,12 @@
+import './jaeeun.css';
+import React from 'react';
+
+function NewComponent() {
+  return (
+    <div>
+      가나다라마바사
+    </div>
+  );
+}
+
+export default NewComponent;
